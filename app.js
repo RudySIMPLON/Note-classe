286612f9ba393072ed2856de756bc76215d6940f
simplon <rudy__10@hotmail.fr>
2016-12-26 19:09:09 +0100

    ajout des fichiers au depot

---
+++ app.js
@@ -0,0 +1,20 @@
+
+
+ var rudy = {George:10, 
+ 	Rudy:20, 
+ 	Cedric:10,
+ 	Arthur: 12
+
+
+
+ };
+
+        for (var prop in rudy) {
+             document.write( prop + " = " + rudy[prop]+ "<br/>");
+
+
+        }
+
+
+
+
